87ef0bdee9afd35e354083b9c3b392c78ee1138c
marci <mia.monteiro@campus.fct.unl.pt>
2025-03-05 17:38:36 +0000

    main structure of the preprocessing of the csv

--- try.py
+++ try.py
@@ -1,1 +1,44 @@
+import pandas as pd
+
+def dataloader():
+    """it loads
+    """
+    pass
+
+def remove_zero_neg_values():
+    """
+    Removes rows where any of the specified columns contain zero or negative values
+    """
+    pass
+
+def remove_duplicates():
+    """
+    Removes duplicated rows in the Dataframe
+    """
+    pass
+
+def remove_missing_values():
+    """
+    Remove rows where any of the specified columns contain missing values
+    """
+    pass
+
+def check_variable_format():
+    """
+    It checks if codes are in the correct format for their specific type
+    """
+    pass
+
+def check_invalid_dates():
+    """Removes entries with invalid date formats
+    """
+
+    pass
+
+def __main__():
+    pass
+
+main()
+
+
 
